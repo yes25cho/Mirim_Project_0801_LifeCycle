c8f49a87af837751c4ba6a629610a1ddc15d663e
yscho010612@gmail.com <yscho010612@gmail.com>
2022-08-01 11:13:34 +0900

    Initial commit

---
+++ app/src/main/java/mediapoint/project/mirim_project_0801_lifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package mediapoint.project.mirim_project_0801_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
